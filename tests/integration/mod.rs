229b5a596c33f645e798320d4fcb380589010411
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-03-10 22:32:24 -0800

    test: move main.rs code to integration test

--- tests/integration/mod.rs
+++ tests/integration/mod.rs
@@ -1,14 +1,18 @@
 use std::path::Path;
 
-mod gvas;
-use crate::gvas::{GVAS, GVASError, GVASReader};
+use unreal_gvas::{
+    GVAS,
+    GVASParser,
+    errors::GVASError,
+};
 
-fn main() {
+#[test]
+fn test_parse_success() {
     let path = Path::new(
         env!("CARGO_MANIFEST_DIR")
     ).join("tests/resources/HL-00-00.sav");
 
-    let gvas: Result<GVAS, GVASError> = GVASReader::parse(&path);
+    let gvas: Result<GVAS, GVASError> = GVASParser::parse(&path);
 
     match gvas {
         Ok(gvas) => println!("{}", gvas),
